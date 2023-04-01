85957de111e43b2004129cbfa9be10ca7de78ab8
221RDB222 <124578710+MartinsBoikovs@users.noreply.github.com>
2023-04-01 16:07:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ class Query:
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-            
- class HashMap:
+
+class PhoneBook:
     def __init__(self):
         self.bucket_count = 1000
-        self.buckets = [[] for _ in range(self.bucket_count)]
+        self._prime = 10000019
         self._multiplier = 263
-        self._prime = 1000000007
+        self.buckets = [[] for _ in range(self.bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -23,28 +23,23 @@ class Query:
     def add(self, string):
         hashed = self._hash_func(str(string.number))
         bucket = self.buckets[hashed]
-        if string.type == 'add':
-            for i in range(len(bucket)):
-                if bucket[i].number == string.number:
-                    bucket[i].name = string.name
-                    break
-            else:
-                self.buckets[hashed] = [string] + bucket
+        if string not in bucket:
+            self.buckets[hashed] = [string] + bucket
 
-    def delete(self, number):
-        hashed = self._hash_func(str(number))
+    def delete(self, string):
+        hashed = self._hash_func(str(string))
         bucket = self.buckets[hashed]
         for i in range(len(bucket)):
-            if bucket[i].number == number:
+            if bucket[i] == string:      
                 bucket.pop(i)
                 break
-             
+                    
     def find(self, string):
         hashed = self._hash_func(string)
         if string in self.buckets[hashed]:
             return "yes"
         return "no"
-
+    
 
 
 def read_queries():
@@ -55,36 +50,23 @@ def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
-    
-    
-    
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    phone_book = PhoneBook()
     for cur_query in queries:
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            phone_book.add(cur_query)
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            phone_book.delete(cur_query.number)
         else:
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            if phone_book.find(cur_query.number) == "yes":
+                for contact in phone_book.buckets[phone_book._hash_func(str(cur_query.number))]:
+                    if contact.number == cur_query.number:
+                        response = contact.name
+                        break
             result.append(response)
     return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
